54ff74e0c927cc1177230a5764db4094727c5661
sandeepkumarreddy <sandeepreddy29356@gmail.com>
2024-09-21 22:00:10 +0530

    added the java bank

---
+++ the-java-bank/src/test/java/com/citi/the_java_bank/TheJavaBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.citi.the_java_bank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheJavaBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
