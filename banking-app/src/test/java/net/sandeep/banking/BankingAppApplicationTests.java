c2455fdc03b247a80ef54b87cef2ec31e38c2340
sandeepkumarreddy <sandeepreddy29356@gmail.com>
2024-09-21 22:10:53 +0530

    added banking app file

---
+++ banking-app/src/test/java/net/sandeep/banking/BankingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package net.sandeep.banking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
